64c2a53deadfcb59ea842eafb547b8bd78b30602
Joey Ezechiels <joey.ezechiels@gmail.com>
2020-02-14 20:46:54 +0100

    Introduce the DeltaError::FailedToEnsure enum variant

--- struct-delta-trait/src/error.rs
+++ struct-delta-trait/src/error.rs
@@ -3,7 +3,14 @@
 
 pub type DeltaResult<T> = Result<T, DeltaError>;
 
-#[derive(Debug)]
+#[derive(Clone, Debug)]
 pub enum DeltaError {
     ExpectedValue,
+    FailedToEnsure {
+        predicate: &'static str,
+        msg: String,
+        file: &'static str,
+        line: u32,
+        column: u32
+    },
 }
