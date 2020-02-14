e24b80ac91fd9d2ad5df7f4e7a817aaee4eb4474
Joey Ezechiels <joey.ezechiels@gmail.com>
2020-02-14 20:47:26 +0100

    Introduce the ensure!() and ensure_{eq,ne,lt,gt,le,ge}!() macros

--- struct-delta-trait/src/error.rs
+++ struct-delta-trait/src/error.rs
@@ -1,6 +1,111 @@
 //!
 
 
+#[macro_export]
+macro_rules! ensure_eq {
+    ($left:expr, $right:expr) => {{
+        let (lstr, rstr) = (stringify!($left), stringify!($right));
+        ensure!(
+            $left == $right,
+            "violation: {} == {}\n{} == {}\n{} == {}",
+            lstr, rstr,
+            lstr, $left,
+            rstr, $right
+        )
+    }};
+}
+
+#[macro_export]
+macro_rules! ensure_ne {
+    ($left:expr, $right:expr) => {{
+        let (lstr, rstr) = (stringify!($left), stringify!($right));
+        ensure!(
+            $left != $right,
+            "violation: {} != {}\n{} == {}\n{} == {}",
+            lstr, rstr,
+            lstr, $left,
+            rstr, $right
+        )
+    }};
+}
+
+#[macro_export]
+macro_rules! ensure_gt {
+    ($left:expr, $right:expr) => {{
+        let (lstr, rstr) = (stringify!($left), stringify!($right));
+        ensure!(
+            $left > $right,
+            "violation: {} > {}\n{} == {}\n{} == {}",
+            lstr, rstr,
+            lstr, $left,
+            rstr, $right
+        )
+    }};
+}
+
+#[macro_export]
+macro_rules! ensure_lt {
+    ($left:expr, $right:expr) => {{
+        let (lstr, rstr) = (stringify!($left), stringify!($right));
+        ensure!(
+            $left < $right,
+            "violation: {} < {}\n{} == {}\n{} == {}",
+            lstr, rstr,
+            lstr, $left,
+            rstr, $right
+        )
+    }};
+}
+
+#[macro_export]
+macro_rules! ensure_ge {
+    ($left:expr, $right:expr) => {{
+        let (lstr, rstr) = (stringify!($left), stringify!($right));
+        ensure!(
+            $left >= $right,
+            "violation: {} >= {}\n{} == {}\n{} == {}",
+            lstr, rstr,
+            lstr, $left,
+            rstr, $right
+        )
+    }};
+}
+
+#[macro_export]
+macro_rules! ensure_le {
+    ($left:expr, $right:expr) => {{
+        let (lstr, rstr) = (stringify!($left), stringify!($right));
+        ensure!(
+            $left <= $right,
+            "violation: {} <= {}\n{} == {}\n{} == {}",
+            lstr, rstr,
+            lstr, $left,
+            rstr, $right
+        )
+    }};
+}
+
+#[macro_export]
+macro_rules! ensure {
+    ($predicate:expr $(, $fmt:expr $(, $args:expr)*)? ) => {
+        if $predicate {
+            $crate::error::DeltaResult::Ok(())
+        } else {
+            Err($crate::error::DeltaError::FailedToEnsure {
+                predicate: stringify!($predicate),
+                msg: {
+                    #[allow(unused)] let mut msg = String::new();
+                    $(  msg = format!($fmt $(, $args)*);  )?
+                    msg
+                },
+                file: file!(),
+                line: line!(),
+                column: column!(),
+            })
+        }
+    };
+}
+
 pub type DeltaResult<T> = Result<T, DeltaError>;
 
 #[derive(Clone, Debug)]
