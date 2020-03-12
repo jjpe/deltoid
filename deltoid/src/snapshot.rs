334f95e1395858c2575e6d38f8bc91dc08cece35
Joey Ezechiels <joey.ezechiels@gmail.com>
2020-03-12 22:37:57 +0100

    Build out snapshotting infrastructure
    * Replace the `Snapshot<T>` type with `DeltaSnapshot<T>` and
      `FullSnapshot<T>`
    * The initial state for a type is simply whatever `Default::default()`
      returns
    * Therefore, the `History<T>` type no longer allows modifying the
      initial state.

--- deltoid/src/snapshot.rs
+++ deltoid/src/snapshot.rs
@@ -11,22 +11,18 @@ macro_rules! snap {
         #[cfg(feature = "snapshot")]
         #[allow(redundant_semicolon)] {
             use $crate::Deltoid;
-            use $crate::snapshot::Snapshot;
+            use $crate::snapshot::DeltaSnapshot;
             let mut origin = String::new();
             $(
                 if !origin.is_empty() { origin.push_str("::"); }
                 origin.push_str(stringify!($location));
             )* ;
-            let context = $context;
-            let history: &mut History<_> = &mut *context.history_mut()?;
-            let old = history.current_state();
+            let history: &mut History<_> = &mut *$context.history_mut()?;
+            let old: &_ = history.current_state();
             let new: &_ = &$new;
             let delta = old.delta(new)?;
-            if history.is_empty() {
-                history.set_initial_state(new.clone());
-            }
-            history.set_current_state(new.clone());
-            history.add_snapshot(Snapshot::new(origin, delta));
+            history.set_current_state(origin.clone(), new);
+            history.add_snapshot(DeltaSnapshot::new(origin, delta));
         }
         #[cfg(not(feature = "snapshot"))] {
             let _ = $context;
@@ -37,87 +33,94 @@ macro_rules! snap {
 
 
 #[cfg(feature = "snapshot")]
-#[derive(
-    Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Default,
-    Deserialize, Serialize
-)]
+#[derive(Debug, Clone, PartialEq, Eq, PartialOrd, Ord, Deserialize, Serialize)]
 pub struct History<T: Deltoid> {
-    snapshots:  Vec<Snapshot<T>>,
-    initial_state: T,
-    current_state: T,
+    snapshots: Vec<DeltaSnapshot<T>>,
+    initial: FullSnapshot<T>,
+    current: FullSnapshot<T>,
 }
 
 #[cfg(feature = "snapshot")]
 impl<T: Deltoid + Default> History<T> {
     pub fn new() -> Self {
         Self {
-            snapshots:     vec![],
-            initial_state: <T>::default(),
-            current_state: <T>::default(),
+            snapshots: vec![],
+            initial: FullSnapshot::default(),
+            current: FullSnapshot::default(),
         }
     }
 
     pub fn reset(&mut self) { *self = Self::new(); }
 
-    pub fn is_empty(&self) -> bool {
-        self.snapshot_count() == 0
-    }
+    pub fn is_empty(&self) -> bool { self.snapshot_count() == 0 }
 
-    pub fn snapshot_count(&self) -> usize {
-        self.snapshots.len()
-    }
+    pub fn snapshot_count(&self) -> usize { self.snapshots.len() }
 
-    pub fn snapshots(&self) -> impl Iterator<Item = &Snapshot<T>> {
-        self.snapshots.iter()
-    }
-
-    pub fn add_snapshot(&mut self, snapshot: Snapshot<T>) {
+    pub fn add_snapshot(&mut self, snapshot: DeltaSnapshot<T>) {
         self.snapshots.push(snapshot);
     }
 
-    pub fn take_snapshots<'s>(&'s mut self) -> impl Iterator<Item = Snapshot<T>> + 's {
+    pub fn snapshots(&self) -> impl Iterator<Item = &DeltaSnapshot<T>> {
+        self.snapshots.iter()
+    }
+
+    pub fn take_snapshots<'s>(&'s mut self) ->
+        impl Iterator<Item = DeltaSnapshot<T>> + 's
+    {
         self.snapshots.drain(..)
     }
 
-    pub fn initial_state(&self) -> &T { &self.initial_state }
+    pub fn initial(&self) -> &FullSnapshot<T> { &self.initial }
 
-    pub fn set_initial_state(&mut self, state: &T) {
-        self.initial_state = state.clone();
-    }
+    pub fn initial_state(&self) -> &T { &self.initial.state }
 
-    pub fn current_state(&self) -> &T { &self.current_state }
+    pub fn current(&self) -> &FullSnapshot<T> { &self.current }
 
-    pub fn set_current_state(&mut self, state: &T) {
-        self.current_state = state.clone();
+    pub fn current_state(&self) -> &T { &self.current.state }
+
+    pub fn set_current_state(&mut self, origin: String, state: &T) {
+        self.current.state = state.clone();
+        self.current.origin = origin;
+        self.current.timestamp = Utc::now();
     }
 }
+
+#[cfg(feature = "snapshot")]
+impl<T: Deltoid + Default> Default for History<T> {
+    fn default() -> Self { Self::new() }
+}
+
 
 
 
 #[cfg(feature = "snapshot")]
 #[derive(Clone, Debug, Deserialize, Serialize)]
-pub struct Snapshot<T: Deltoid> {
+pub struct FullSnapshot<T: Deltoid> {
     pub timestamp: DateTime<Utc>,
     pub origin: String,
-    pub contents: <T as Deltoid>::Delta,
+    pub state: T,
 }
 
 #[cfg(feature = "snapshot")]
-impl<T: Deltoid> Snapshot<T> {
-    pub fn new(
-        origin: String,
-        contents: <T as Deltoid>::Delta
-    ) -> Self {
+impl<T: Deltoid> FullSnapshot<T> {
+    pub fn new(origin: String, state: T) -> Self {
+        Self { timestamp: Utc::now(), origin, state }
+    }
+}
+
+#[cfg(feature = "snapshot")]
+impl<T: Deltoid + Default> Default for FullSnapshot<T> {
+    fn default() -> Self {
         Self {
             timestamp: Utc::now(),
-            origin: origin,
-            contents: contents,
+            origin: "default".to_string(),
+            state: Default::default(),
         }
     }
 }
 
 #[cfg(feature = "snapshot")]
-impl<T: Deltoid> PartialEq for Snapshot<T> {
+impl<T: Deltoid> PartialEq for FullSnapshot<T> {
     fn eq(&self, rhs: &Self) -> bool {
         if self.timestamp != rhs.timestamp { return false; }
         if self.origin != rhs.origin { return false; }
@@ -126,10 +129,10 @@ impl<T: Deltoid> PartialEq for Snapshot<T> {
 }
 
 #[cfg(feature = "snapshot")]
-impl<T: Deltoid> Eq for Snapshot<T> {}
+impl<T: Deltoid> Eq for FullSnapshot<T> {}
 
 #[cfg(feature = "snapshot")]
-impl<T: Deltoid> PartialOrd for Snapshot<T> {
+impl<T: Deltoid> PartialOrd for FullSnapshot<T> {
     fn partial_cmp(&self, rhs: &Self) -> Option<Ordering> {
         let timestamp_cmp = self.timestamp.partial_cmp(&rhs.timestamp);
         if timestamp_cmp != Some(Ordering::Equal) { return timestamp_cmp }
@@ -140,7 +143,59 @@ impl<T: Deltoid> PartialOrd for Snapshot<T> {
 }
 
 #[cfg(feature = "snapshot")]
-impl<T: Deltoid> Ord for Snapshot<T> {
+impl<T: Deltoid> Ord for FullSnapshot<T> {
+    fn cmp(&self, rhs: &Self) -> Ordering {
+        let timestamp_cmp = self.timestamp.cmp(&rhs.timestamp);
+        if timestamp_cmp != Ordering::Equal { return timestamp_cmp }
+        let origin_cmp = self.origin.cmp(&rhs.origin);
+        if origin_cmp != Ordering::Equal { return origin_cmp }
+        Ordering::Equal
+    }
+}
+
+
+
+
+#[cfg(feature = "snapshot")]
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct DeltaSnapshot<T: Deltoid> {
+    pub timestamp: DateTime<Utc>,
+    pub origin: String,
+    pub delta: <T as Deltoid>::Delta,
+}
+
+#[cfg(feature = "snapshot")]
+impl<T: Deltoid> DeltaSnapshot<T> {
+    pub fn new(origin: String, delta: <T as Deltoid>::Delta) -> Self {
+        Self { timestamp: Utc::now(), origin, delta }
+    }
+}
+
+#[cfg(feature = "snapshot")]
+impl<T: Deltoid> PartialEq for DeltaSnapshot<T> {
+    fn eq(&self, rhs: &Self) -> bool {
+        if self.timestamp != rhs.timestamp { return false; }
+        if self.origin != rhs.origin { return false; }
+        true
+    }
+}
+
+#[cfg(feature = "snapshot")]
+impl<T: Deltoid> Eq for DeltaSnapshot<T> {}
+
+#[cfg(feature = "snapshot")]
+impl<T: Deltoid> PartialOrd for DeltaSnapshot<T> {
+    fn partial_cmp(&self, rhs: &Self) -> Option<Ordering> {
+        let timestamp_cmp = self.timestamp.partial_cmp(&rhs.timestamp);
+        if timestamp_cmp != Some(Ordering::Equal) { return timestamp_cmp }
+        let origin_cmp = self.origin.partial_cmp(&rhs.origin);
+        if origin_cmp != Some(Ordering::Equal) { return origin_cmp }
+        Some(Ordering::Equal)
+    }
+}
+
+#[cfg(feature = "snapshot")]
+impl<T: Deltoid> Ord for DeltaSnapshot<T> {
     fn cmp(&self, rhs: &Self) -> Ordering {
         let timestamp_cmp = self.timestamp.cmp(&rhs.timestamp);
         if timestamp_cmp != Ordering::Equal { return timestamp_cmp }
