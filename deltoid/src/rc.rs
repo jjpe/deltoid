aa7660d3f28fcf26d5067bd398d1ffb00bfbf8ee
Joey Ezechiels <joey.ezechiels@gmail.com>
2020-04-07 04:18:07 +0200

    Improve the delta calculated by <Rc<_> as Deltoid>::delta()

--- deltoid/src/rc.rs
+++ deltoid/src/rc.rs
@@ -17,21 +17,29 @@ where T: Deltoid + PartialEq + Clone + std::fmt::Debug
 
     fn apply_delta(&self, delta: &Self::Delta) -> DeltaResult<Self> {
         let lhs: &T = self.as_ref();
-        let rhs: &<T as Deltoid>::Delta = &delta.0;
-        lhs.apply_delta(rhs).map(Rc::new)
+        match &delta.0 {
+            None => Ok(self.clone()),
+            Some(delta) => lhs.apply_delta(delta).map(Rc::new),
+        }
     }
 
     fn delta(&self, rhs: &Self) -> DeltaResult<Self::Delta> {
         let lhs: &T = self.as_ref();
         let rhs: &T = rhs.as_ref();
-        lhs.delta(rhs).map(Box::new).map(RcDelta)
+        Ok(RcDelta(if lhs == rhs {
+            None
+        } else {
+            Some(Box::new(lhs.delta(rhs)?))
+        }))
     }
 }
 
 
 #[derive(Clone, Debug, PartialEq)]
 #[derive(serde_derive::Deserialize, serde_derive::Serialize)]
-pub struct RcDelta<T: Deltoid>(#[doc(hidden)]pub Box<<T as Deltoid>::Delta>);
+pub struct RcDelta<T: Deltoid>(
+    #[doc(hidden)] pub Option<Box<<T as Deltoid>::Delta>>
+);
 
 
 impl<T> IntoDelta for Rc<T>
@@ -41,16 +49,19 @@ where T: Deltoid + IntoDelta
 {
     fn into_delta(self) -> DeltaResult<<Self as Deltoid>::Delta> {
         let thing: T = self.as_ref().clone();
-        thing.into_delta().map(Box::new).map(RcDelta)
+        thing.into_delta().map(Box::new).map(Some).map(RcDelta)
     }
 }
 
 impl<T> FromDelta for Rc<T>
-where T: Deltoid + FromDelta
+where T: Deltoid + FromDelta + Default
     + for<'de> serde::Deserialize<'de>
     + serde::Serialize
 {
     fn from_delta(delta: <Self as Deltoid>::Delta) -> DeltaResult<Self> {
-        <T>::from_delta(*delta.0).map(Rc::new)
+        match delta.0 {
+            None => Ok(Self::default()),
+            Some(delta) => <T>::from_delta(*delta).map(Rc::new),
+        }
     }
 }
