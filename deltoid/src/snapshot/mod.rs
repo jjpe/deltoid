7d63c23dad6e44ee936e2bf79c11984a7e652834
Joey Ezechiels <joey.ezechiels@gmail.com>
2021-03-06 13:22:47 +0100

    Refactor the snapshot! macro
    In particular, refactor it to use the deltoid::snapshot::snapshot()
    function, which is also introduced by this commit.  This makes the
    macro code more type-safe, and provides better errors.

--- deltoid/src/snapshot/mod.rs
+++ deltoid/src/snapshot/mod.rs
@@ -3,6 +3,8 @@
 #[cfg(feature = "snapshot")] mod delta;
 #[cfg(feature = "snapshot")] mod full;
 
+#[cfg(feature = "snapshot")] use crate::core::{Apply, Core, Delta};
+#[cfg(feature = "snapshot")] use crate::error::DeltaError;
 #[cfg(feature = "snapshot")] pub use crate::snapshot::delta::*;
 #[cfg(feature = "snapshot")] pub use crate::snapshot::full::*;
 
@@ -18,17 +20,39 @@ where T: crate::Core + Default {
     }
 }
 
+#[cfg(feature = "snapshot")]
+pub fn snapshot<S, E>(
+    ctx: &mut dyn SnapshotCtx<S>,
+    origin: String,
+    msg: Option<String>,
+    new_state: &S,
+) -> Result<(), E>
+where
+    S: Delta + Apply + Default,
+    E: From<DeltaError>
+{
+    let history: &mut DeltaSnapshots<S> = ctx.history();
+    let old_state: &S = &history.current().state;
+    let timestamp = history.current().timestamp.clone();
+    let delta: <S as Core>::Delta = match old_state.delta(new_state) {
+        Ok(delta) => delta,
+        Err(derr) => return Err(derr.into()),
+    };
+    history.update_current(origin.clone(), new_state);
+    // let origin = history.current().origin.clone();
+    history.add_snapshot(DeltaSnapshot { timestamp, origin, msg, delta });
+    Ok(())
+}
+
 #[macro_export]
 macro_rules! snapshot {
     (
         use result type $result_type:ty;
-        [$($origin:ident)::*] $new_state:expr => $context:expr
+        [$($origin:ident)::*] $($new_state:expr),+ => $ctx:expr
         $(; $fmt:expr $(, $arg:expr)* )?
-    ) => { loop /* used as a do-block rather than a loop */ {
+    ) => { loop {
         #[cfg(feature = "snapshot")]
         #[allow(redundant_semicolons, unused)] {
-            use deltoid::{Core, Apply, Delta, FromDelta, IntoDelta};
-            use deltoid::snapshot::{DeltaSnapshot, DeltaSnapshots, SnapshotCtx};
             let mut origin = String::new();
             $(
                 if !origin.is_empty() { origin.push_str("::"); }
@@ -38,36 +62,30 @@ macro_rules! snapshot {
             $(
                 msg = Some(format!($fmt $(, $arg)*));
             )? ;
-            let ctx: &mut SnapshotCtx<_> = $context;
-            let mut history_guard = match ctx.history() {
-                Ok(guard) => guard,
-                Err(err) => break Err(err.into()) as $result_type,
-            };
-            let history: &mut DeltaSnapshots<_> = &mut *history_guard;
-            let old_state: &_ = &history.current().state;
-            let new_state: &_ = &$new_state;
-            let delta = match old_state.delta(new_state) {
-                Ok(delta) => delta,
-                Err(derr) => break Err(derr.into()) as $result_type,
-            };
-            history.update_current(origin, new_state);
-            history.add_snapshot(DeltaSnapshot {
-                timestamp: history.current().timestamp.clone(),
-                origin:    history.current().origin.clone(),
-                msg,
-                delta,
-            });
+            $(
+                let result = $crate::snapshot::snapshot(
+                    $ctx,
+                    origin.clone(),
+                    msg.clone(),
+                    $new_state
+                );
+                if let Err(err) = result {
+                    break Err(err);
+                }
+            )+ ;
         }
         #[cfg(not(feature = "snapshot"))] {
-            let _ = $new_state;
-            let _ = $context;
+            $(
+                let _ = $new_state;
+            )+ ;
+            let _ = $ctx;
             $(
                 let _ = $fmt;
                 $(
                     let _ = $arg;
                 )*
-            )?
+            )? ;
         }
-        break Ok(()) as $result_type;
+        break Ok(()) as $result_type
     }}
 }
